18345f26f721497d7a96080d51ee2c9fd52a4528
Eyali1001 <eyal.merhavi@gmail.com>
2016-08-28 21:38:21 +0300

    0.51

---
+++ commands.py
@@ -0,0 +1,3 @@
+r = requests.post("https://dc018499.ngrok.io/signup",data={'t':'p','u':'eyal mer','p':'1234','b':s,'a':16,'g':'m'})
+
+ r = requests.post("https://dc018499.ngrok.io/",data={'u':'eyal mer','d':'very sick','c':'general'})
